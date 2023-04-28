0d4bbf054224aeda7419781c82d84f89c99c9e70
Paola E. Lopez <paolaeslo23@gmail.com>
2023-04-28 09:29:30 -0500

    Primer repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraAngular';
+  title = 'Bienvenido a la Mini Calculadora de Angular';
+//crenado variables de clase
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  //crenado un metodo Suma
+  sumar():void{
+    this.resultado= this.numero1 + this.numero2;
+  }
+  restar():void{
+    this.resultado= this.numero1 - this.numero2;
+  }
+  multiplicar():void{
+    this.resultado= this.numero1 * this.numero2;
+  }
+  dividir():void{
+    this.resultado= this.numero1 / this.numero2;
+  }
+
 }
